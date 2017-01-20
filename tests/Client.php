4e3d0fd47df35d65bc78cc7c043eb254b01549b6
Dominik Angerer <dominikangerer1@gmail.com>
2017-01-20 16:13:31 +0100

    switched to shared Test Space added UiKit Styles formatted output added DataSource Test Cases

--- tests/Client.php
+++ tests/Client.php
@@ -1,22 +1,29 @@
 <?php
 require '../vendor/autoload.php';
 
-$client = new \Storyblok\Client('t618GfLe1YHICBioAHnMrwtt', 'localhost:3001');
+$client = new \Storyblok\Client('Iw3XKcJb6MwkdZEwoQ9BCQtt'); //
 
 // Optionally set a cache
 $client->setCache('filesytem', array('path' => 'cache'));
 
 // Get the story as array
-$client->getStoryBySlug('demo');
+$client->getStoryBySlug('home');
 $data = $client->getBody();
 
-var_dump($data);
 
+echo '<link href="https://getuikit.com/css/theme.css?174" rel="stylesheet" type="text/css">';
+echo '<div class="uk-container uk-container-center uk-margin-large-top uk-margin-large-bottom">
+				<h1> PHP Client Runable Test </h1>
+				<h2> getStoryBySlug "home" </h2>
+				<pre>';
+var_dump($data);
+echo '	</pre>';
 echo '<hr>';
 
 $tree = $client->editMode()->getLinks()->getAsTree();
 
-echo '<ul>';
+echo '<h2> getLinks | getAsTree </h2>
+			<ul>';
 foreach ($tree as $item) {
 	echo '<li>' . $item['item']['name'];
 
@@ -32,20 +39,46 @@ foreach ($tree as $item) {
 }
 echo '</ul>';
 
+echo '<h2> Check: $client->cacheVersion </h2>
+			<pre>';
 var_dump($client->cacheVersion);
-
-echo '<hr>';
+echo '</pre>
+			<hr>
+			<p> Wait 2 Seconds </p>';
 
 sleep(2);
 
-echo '<hr>';
-
+echo '<pre>';
 var_dump($client->cacheVersion);
+echo '</pre>
+			<hr>
+			<h2> deleteCacheBySlug "home" | flushCache </h2>';
 
-echo '<hr>';
+$client->deleteCacheBySlug('home');
 
-$client->deleteCacheBySlug('demo');
-
+echo '<pre>';
 var_dump($client->cacheVersion);
+echo '</pre>';
 
 $client->flushCache();
+
+echo '<hr>';
+
+$client->getDatasourceEntries('test-case-1');
+$data = $client->getBody();
+
+echo '<h2> getDatasourceEntries "test-case-1" | getBody </h2>
+			<pre>';
+var_dump($data);
+echo '</pre>';
+echo '<hr>';
+
+$data = $client->getAsNameValueArray();
+
+echo '<h2> getDatasourceEntries "test-case-1" | getAsNameValueArray </h2>
+			<pre>';
+var_dump($data);
+echo '</pre>';
+echo '<hr>';
+
+echo '</div>';
