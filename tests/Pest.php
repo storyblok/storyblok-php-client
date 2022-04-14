5a15b84831bba629d99b909a321065845f8a64ee
João Gonçalves <joao@kamun.com.br>
2022-04-14 14:23:02 +0100

    remove unused function
    fix

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,22 +1,10 @@
 <?php
 
 use GuzzleHttp\Psr7\Response;
-use Storyblok\Client;
 
 function mockResponse($endpoint = 'stories', $headers = [], $version = 'v2', $statusCode = 200)
 {
     $content = file_get_contents("./tests/Data/{$version}/{$endpoint}.json");
 
     return new Response($statusCode, $headers, $content);
-}
-
-function mockClient($endpoint, $token, $version = 'v2')
-{
-    $mocks = [
-        new Response(200, ['server' => 'nginx/1.18.0'], ),
-    ];
-
-    return (new Client($token, null, $version))
-        ->mockable($mocks, $version)
-    ;
 }
