3ead63c0232ed137ebd7b26b18a838e201dee506
Roberto B <roberto.butti@gmail.com>
2022-04-19 13:20:10 +0200

    Update .php-cs-fixer.php

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,10 +1,8 @@
 <?php
 
 $finder = PhpCsFixer\Finder::create()
-//$finder = Symfony\Component\Finder\Finder::create()
     ->in([
         __DIR__ . '/src',
-        //__DIR__ . '/tests',
     ])
     ->name('*.php')
     ->ignoreDotFiles(true)
